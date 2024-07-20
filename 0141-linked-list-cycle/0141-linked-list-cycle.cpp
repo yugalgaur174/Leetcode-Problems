b6cc8a7a0b27bedde7f6a351cdb47d9166a7d425
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-07-20 12:17:45 +0530

    Time: 8 ms (60.79%), Space: 10.7 MB (37.43%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,19 +11,25 @@ public:
     bool hasCycle(ListNode *head) {
         if(head==NULL) return 0;
         if(head->next==NULL) return 0;
-
-        unordered_map<ListNode*,int> mpp;
-        ListNode* mover=head;
-        while(mover){
-            if(mpp[mover]==0){
-                mpp[mover]=1;
-
-            }
-            else{
-                return 1;
-            }
-            mover=mover->next;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next && fast->next->next){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow) return 1;
         }
+        // unordered_map<ListNode*,int> mpp;
+        // ListNode* mover=head;
+        // while(mover){
+        //     if(mpp[mover]==0){
+        //         mpp[mover]=1;
+
+        //     }
+        //     else{
+        //         return 1;
+        //     }
+        //     mover=mover->next;
+        // }
         return 0;
     }
 };
