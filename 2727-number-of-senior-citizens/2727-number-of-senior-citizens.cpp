e6f1cbb429ff64684088873fad7f8fb413793a64
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-08-01 22:16:20 +0530

    Time: 7 ms (50.4%), Space: 17.8 MB (16.71%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int countSeniors(vector<string>& details) {
         int n=0;
-        for(int i=0;i<details.size();i++){
-            int a=stoi(details[i].substr(11,2));
+        for(auto i:details){
+            int a=stoi(i.substr(11,2));
             if(a>60){
                 n++;
             }
