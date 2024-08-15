3d85532c3431052f4f93e6287d2e39c25e1a8ede
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-08-16 00:16:42 +0530

    Time: 19 ms (80.83%), Space: 28.1 MB (8.94%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int i=0;
-        vector<int> a;
-        int max=nums[i];
-        for(int j=0;j<nums.size();j++){
-            if(nums[j]>max){
-                max=nums[j];
-            }
+        stack<int> st;
+        int n=nums.size();
+        vector<int> a(n,-1);
+        for(int i=0;i<n;i++){
+            nums.push_back(nums[i]);
         }
-        for(int j=0;j<nums.size();j++){
-            if(nums[j]==max) a.push_back(-1);
-            
-            else{
-                i=(j+1)%nums.size();
-                while(nums[i]<=nums[j]){
-                    // continue;
-                    i=(i+1)%nums.size();
-                }
-                a.push_back(nums[i]);
-                
+        for(int i=2*n-1;i>n-1;i--){
+            while(!st.empty() && st.top()<nums[i]){
+                st.pop();
             }
+            st.push(nums[i]);
+        }
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                // a.push_back(-1);
+                a[i]=-1;
+            }
+            else{
+                // a.push_back(st.top());
+                a[i]=st.top();
+            }
+            st.push(nums[i]);
         }
         return a;
     }
