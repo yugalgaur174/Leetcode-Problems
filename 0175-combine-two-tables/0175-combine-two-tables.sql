5e8a714da9b3b1628cc8b5906c0296761489ef37
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-08-28 17:22:07 +0530

    Time: 380 ms (62.87%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select firstName, lastName, city, state from Person left join Address on Person.personId=Address.personId;
+select Person.firstName, Person.lastName, Address.city, Address.state from Person left join Address on Person.personId=Address.personId;
