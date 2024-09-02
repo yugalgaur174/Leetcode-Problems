5e28bb3820cc6119ffc5e445a3d683ffa1013dbf
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-02 09:46:52 +0530

    Time: 98 ms (5.06%), Space: 60 MB (5.02%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void dfss(unordered_map<int,vector<int>> mpp,int i, vector<int>&visited){
+        visited[i]=1;
+        for(auto it:mpp[i]){
+            if(!visited[it]){
+
+            dfss(mpp,it,visited);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int cnt=0;
+        unordered_map<int,vector<int>> mpp;
+        int n=isConnected.size();
+        vector<int> visited(n,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 && i!=j)mpp[i].push_back(j);
+            }
+        }
+        for(int i=0;i<isConnected.size();i++){
+            if(!visited[i]){
+                cnt++;
+                dfss(mpp,i,visited);
+
+            }
+        }
+        return cnt;
+    }
+};
