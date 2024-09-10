564342c4a568fd70c7d1a68e2e403501b46ec105
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-10 23:23:19 +0530

    Time: 5 ms (52.11%), Space: 17 MB (46.49%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,23 +11,47 @@
  */
 class Solution {
 public:
-vector<TreeNode*> arr;
-    void preorder(TreeNode* node){
-        if(node==nullptr) return;
-        arr.push_back(node);
-        preorder(node->left);
-        preorder(node->right);
-
-    }
+    TreeNode* node=nullptr;
     void flatten(TreeNode* root) {
-        TreeNode* node=root;
-        preorder(node);
-        for(int i=1;i<arr.size();i++){
-            // TreeNode*mover=new TreeNode(arr[i]);
-            node->right=arr[i];
-            node->left=nullptr;
-            node=node->right;
-        }
-        
+        if(root==nullptr) return;
+        flatten(root->right);
+        flatten(root->left);
+        root->right=node;
+        root->left=nullptr;
+        node=root;
     }
 };
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+// vector<TreeNode*> arr;
+//     void preorder(TreeNode* node){
+//         if(node==nullptr) return;
+//         arr.push_back(node);
+//         preorder(node->left);
+//         preorder(node->right);
+
+//     }
+//     void flatten(TreeNode* root) {
+//         TreeNode* node=root;
+//         preorder(node);
+//         for(int i=1;i<arr.size();i++){
+//             // TreeNode*mover=new TreeNode(arr[i]);
+//             node->right=arr[i];
+//             node->left=nullptr;
+//             node=node->right;
+//         }
+        
+//     }
+// };
