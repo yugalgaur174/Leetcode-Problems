46f20a0afe6c78a11c6246c5a94e6145c8b539aa
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-11 01:06:08 +0530

    Time: 5 ms (52.02%), Space: 17.1 MB (20.52%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -9,18 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    TreeNode* node=nullptr;
     void flatten(TreeNode* root) {
-        if(root==nullptr) return;
-        flatten(root->right);
-        flatten(root->left);
-        root->right=node;
-        root->left=nullptr;
-        node=root;
+        if(root==nullptr) return ;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* curr=st.top();
+            st.pop();
+            if(curr->right!=nullptr) st.push(curr->right);
+            if(curr->left!=nullptr) st.push(curr->left);
+            if(!st.empty()) curr->right=st.top();
+            curr->left=nullptr;
+        }
     }
 };
+
+// class Solution {
+// public:
+//     TreeNode* node=nullptr;
+//     void flatten(TreeNode* root) {
+//         if(root==nullptr) return;
+//         flatten(root->right);
+//         flatten(root->left);
+//         root->right=node;
+//         root->left=nullptr;
+//         node=root;
+//     }
+// };
 
 // /**
 //  * Definition for a binary tree node.
