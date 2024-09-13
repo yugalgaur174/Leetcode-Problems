b10dfe9f608422eeb5f9985cfc980af8d8c14359
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-13 22:11:31 +0530

    Time: 59 ms (65.18%), Space: 42 MB (57.06%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        vector<int> a;
-        for(int i=0;i<queries.size();i++){
-            int b=0;
-            for(int j=queries[i][0];j<queries[i][1]+1;j++){
-                b=b^arr[j];
-            }
-            a.push_back(b);
+        vector<int> a,c;
+        int aa[arr.size()];
+        int b=0;
+        for(int i=0;i<arr.size();i++){
+            b=b^arr[i];
+            aa[i]=b;
         }
-        // a.push_back(3^0);
-        return a;
+        for(int i=0;i<queries.size();i++){
+            if(queries[i][0]>0) c.push_back(aa[queries[i][0]-1]^aa[queries[i][1]]);
+            else c.push_back(aa[queries[i][1]]);
+        }
+        // for(int i=0;i<queries.size();i++){
+        //     int b=0;
+        //     for(int j=queries[i][0];j<queries[i][1]+1;j++){
+        //         b=b^arr[j];
+        //     }
+        //     a.push_back(b);
+        // }
+        // // a.push_back(3^0);
+        return c;
     }
 };
