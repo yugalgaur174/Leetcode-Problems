55be51a27da5978d1a3a23aaae764d757f428e61
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-20 00:06:48 +0530

    Time: 0 ms (100%), Space: 10.3 MB (61.17%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        set<int> st;
+        // set<int> st;
+        sort(nums.begin(),nums.end());
+        priority_queue<int> pq;
         for(int i=0;i<nums.size();i++){
             if(nums[i]>0){
-
-            st.insert(nums[i]);
+                if(pq.empty()){
+                    pq.push(nums[i]);
+                }
+                else{
+                    if(pq.top()==nums[i]) continue;
+                    else{
+                        pq.push(nums[i]);
+                    }
+                }
+            
             }
         }
-        return st.size();
+        return pq.size();
     }
 };
