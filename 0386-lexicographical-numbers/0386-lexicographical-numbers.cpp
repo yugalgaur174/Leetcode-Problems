a928de27e23c96dd440cb6c94df96550bdd9fd83
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-21 12:01:40 +0530

    Time: 472 ms (5.05%), Space: 16.9 MB (34.93%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    static bool lexo(int a, int b){
+
+        return to_string(a)<to_string(b);
+    }
+    vector<int> lexicalOrder(int n) {
+        vector<int> arr;
+        for(int i=1;i<=n;i++){
+            arr.push_back(i);
+        }
+        sort(arr.begin(),arr.end(),lexo);
+        return arr;
+    }
+};
