c94e151d35df16d77e2200afe05ad43170a72a7b
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-22 17:32:03 +0530

    Time: 40 ms (19.68%), Space: 47 MB (14.54%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    void permu(int ind,int target,vector<int>& candidates,vector<vector<int>>& ans,vector<int>& a){
-        if(ind==candidates.size()){
-            if(target==0){
-                ans.push_back(a);            }
-        return;
+    void findcombination(int ind, int target, vector<int> candidates,
+                         vector<vector<int>>& ans, vector<int>& ds) {
+        if (ind == candidates.size()) {
+            if (target == 0) {
+                ans.push_back(ds);
+            }
+            return;
         }
-        if(candidates[ind]<=target){
-            a.push_back(candidates[ind]);
-            permu(ind,target-candidates[ind],candidates,ans,a);
-            a.pop_back();
+        if (candidates[ind] <= target) {
+            ds.push_back(candidates[ind]);
+            findcombination(ind, target - candidates[ind], candidates, ans, ds);
+            ds.pop_back();
         }
-        permu(ind+1,target,candidates,ans,a);
+        findcombination(ind + 1, target, candidates, ans, ds);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> a;
+        vector<int> ds;
         vector<vector<int>> ans;
-        permu(0,target,candidates,ans,a);
+        findcombination(0, target, candidates, ans, ds);
         return ans;
     }
 };
