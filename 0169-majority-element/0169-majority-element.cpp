ea3332551d577f12e40fb77c7aa62a3ccb38741a
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-22 14:57:30 +0530

    Time: 12 ms (88.78%), Space: 26.9 MB (79.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,18 +2,33 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
+        int cl=0;
+        int cnt=0;
         for(int i=0;i<n;i++){
-            int k=0;
-            for(int j=0;j<n;j++){
-                if(nums[i]==nums[j]){
-                    k++;
-                }
-
+            if(cnt==0){
+                cl=nums[i];
+                cnt=1;
             }
-            if(k>n/2){
-                return nums[i];
+            else if(cl==nums[i]){
+                cnt++;
+            }
+            else if(cl!=nums[i]){
+                cnt--;
             }
         }
+        return cl;
+        // for(int i=0;i<n;i++){
+        //     int k=0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[i]==nums[j]){
+        //             k++;
+        //         }
+
+        //     }
+        //     if(k>n/2){
+        //         return nums[i];
+        //     }
+        // }
         // set <int> t;
         // for(int i=0;i<n;i++){
         //     t.insert(nums[i]);
