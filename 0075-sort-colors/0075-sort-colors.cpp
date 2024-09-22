dadd86aab8d965a543ff3243188d0b7ec988987e
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-22 13:02:10 +0530

    Time: 0 ms (100%), Space: 10.1 MB (91.87%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            int j=i-1;
+            int x=nums[i];
+            while(j>=0&&nums[j]>x ){
+                nums[j+1]=nums[j];
+                j--;
+            }
+            nums[j+1]=x;
+        }
         for(int i=0;i<nums.size();i++){
             cout<<nums[i];
         }
