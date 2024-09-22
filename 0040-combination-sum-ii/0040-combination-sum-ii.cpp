a50e3823a72a84c9711f11e0778804d2bbca2d78
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-22 17:20:50 +0530

    Time: 3 ms (75.71%), Space: 16.4 MB (13.1%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,26 +1,23 @@
 class Solution {
- 	int target_;
- 	void recursion(vector<int>& candidates, int index, int sum, vector<int>& cur, vector<vector<int>>& res_){
- 		if(target_ == sum) res_.push_back(cur);
- 		else if(sum < target_){
- 			int size = candidates.size();
- 			for(int i = index; i < size; ++i){
- 				while(i > index && i < size && candidates[i] == candidates[i - 1]) ++i;
- 				if(i >= size) break;
- 				cur.emplace_back(candidates[i]);
- 				recursion(candidates, i + 1, sum + candidates[i], cur, res_);
- 				cur.pop_back();
- 			}
- 		}
- 	}
- public:
- 	vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
- 		if(candidates.empty()) return {};
- 		sort(candidates.begin(), candidates.end());
- 		vector<vector<int>> res_;   
- 		vector<int> cur;
- 		target_ = target;
- 		recursion(candidates, 0, 0, cur, res_);
- 		return res_;
- 	}
- };
+public:
+    void findcombination(int ind,int target, vector<int> candidates,vector<vector<int>>& ans, vector<int> & ds){
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=ind;i<candidates.size();i++){
+            if(i>ind && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) break;
+            ds.push_back(candidates[i]);
+            findcombination(i+1,target-candidates[i],candidates,ans,ds);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> ds;
+        vector<vector<int>> ans;
+        sort(candidates.begin(),candidates.end());
+        findcombination(0,target,candidates,ans, ds);
+        return ans;
+    }
+};
