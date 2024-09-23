af6f1982704fe2e2efa5521fe9ae614ccb31c720
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-23 12:40:45 +0530

    Time: 9 ms (57.38%), Space: 16.9 MB (50.17%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,23 +3,50 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<int> col(m,0);
-        vector<int> row(n,0);
+        // vector<int> col(m,0);
+        // vector<int> row(n,0);
+        int col0=1;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    col[j]=1;
-                    row[i]=1;
+                    matrix[i][0]=0;
+                    if(j==0){
+                       col0=0; 
+                    }
+                    else{
+
+                    matrix[0][j]=0;
+                    }
                 }
             }
         }
-        for(int i=0;i<n;i++){
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+        if(matrix[0][0]==0){
             for(int j=0;j<m;j++){
-                if(row[i] || col[j]){
-                    matrix[i][j]=0;
-                }
+                matrix[0][j]=0;
             }
         }
-        
+        if(!col0){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
+            }
+        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(row[i] || col[j]){
+        //             matrix[i][j]=0;
+        //         }
+        //     }
+        // }
+
     }
 };
