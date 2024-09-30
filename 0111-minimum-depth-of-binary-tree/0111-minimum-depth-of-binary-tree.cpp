a23d1650eb4114a6a2d195affe1a5e44c19bc8b7
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-09-30 14:43:29 +0530

    Time: 181 ms (74.25%), Space: 144.7 MB (99.02%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -10,27 +10,25 @@
  * };
  */
 class Solution {
+
 public:
+    int ans=INT_MAX;
+    void solution(TreeNode* node, int no){
+        if(node->left==nullptr && node->right==nullptr) {
+            // ans=no;
+            if(ans>no)ans=no;
+            return;
+        }
+        if(node->left){
+            solution(node->left,no+1);
+        }
+        if(node->right){
+            solution(node->right,no+1);
+        }
+    }
     int minDepth(TreeNode* root) {
         if(root==nullptr) return 0;
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,1});
-        while(!q.empty()){
-           auto it=q.front();
-            q.pop();
-            TreeNode* node=it.first;
-            int no=it.second;
-            if(node->left==nullptr && node->right==nullptr) {
-                return no;
-                break;
-            }
-            if(node->left){
-                q.push({node->left,no+1});
-            }
-            if(node->right){
-                q.push({node->right,no+1});
-            }
-        }
-        return 0;
+        solution(root,1);
+        return ans;
     }
 };
