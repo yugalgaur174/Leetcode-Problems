6f5901aee499ff88d1f19f01d658cc8366987d09
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-10-04 16:29:38 +0530

    Time: 150 ms (35.78%), Space: 74.9 MB (28.79%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,21 +10,48 @@
  */
 class Solution {
 public:
+   ListNode* merge(ListNode* l1,ListNode* l2){
+        ListNode* dummy=new ListNode(0);
+        ListNode* temp=dummy;
+
+        while(l1&& l2){
+            if(l1->val>l2->val){
+                temp->next=l2;
+                l2=l2->next;
+            }
+            else{
+                temp->next=l1;
+                l1=l1->next;
+            }
+            temp=temp->next;
+        }
+        while(l1){
+            temp->next=l1;
+                l1=l1->next;
+            temp=temp->next;
+        }
+        while(l2){
+            temp->next=l2;
+                l2=l2->next;
+            temp=temp->next;
+
+        }
+        return dummy->next;
+    }
     ListNode* sortList(ListNode* head) {
-        vector<int> ans;
-        ListNode* mover=head;
-        while(mover){
-            ans.push_back(mover->val);
-            mover=mover->next;
+        if(head==nullptr ||  head->next==nullptr) return head;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        ListNode* prev;
+        while(fast && fast->next){
+            prev=slow;
+            fast=fast->next->next;
+            slow=slow->next;
         }
-        sort(ans.begin(),ans.end());
-        mover=head;
-        int i=0;
-        while(mover){
-            mover->val=ans[i];
-            mover=mover->next;
-            i++;
-        }
-        return head;
+        prev->next=nullptr;
+        ListNode* l1= sortList(head);
+        ListNode* l2=sortList(slow);
+        return merge(l1,l2);
+
     }
 };
