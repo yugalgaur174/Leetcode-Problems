dfc0a5f96de6cfc9591cb94b00b43c0e450baf2a
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-10-22 13:04:56 +0530

    Time: 812 ms (9.67%), Space: 170.1 MB (5.08%) - LeetHub

--- 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
@@ -11,20 +11,24 @@
  */
 class Solution {
 public:
-    unordered_map<int, long long> mpp;
+    // unordered_map<int, long long> mpp;
+    
+        vector<long long> ans;
     void check(TreeNode* node, int level){
         if(node==nullptr) return ;
-        mpp[level]+=node->val;
-        check(node->left, level+1);
-        check(node->right,level+1);
+        if(ans.size()<level){
+            ans.push_back(node->val);
+        }
+        else{
+            ans[level-1]+=node->val;
+        }
+        check(node->left,level+1);
+        check(node->right, level+1);
     }
     long long kthLargestLevelSum(TreeNode* root, int k) {
         if(root==nullptr) return 0;
         check(root,1);
-        vector<long long> ans;
-        for(auto& it:mpp){
-            ans.push_back(it.second);
-        }
+        
         sort(ans.begin(),ans.end());
         if(k>ans.size()) return -1;
         return ans[ans.size()-k];
