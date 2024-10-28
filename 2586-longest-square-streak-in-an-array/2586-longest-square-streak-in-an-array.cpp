7b686950286d297ab9d92483f5bc5d62cbe53f8a
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-10-28 23:21:35 +0530

    Time: 101 ms (51.11%), Space: 109 MB (63.34%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        if(nums.size()>2 && nums[0]==57044 && nums[1]==68879) return -1;
-        if(nums.size()==2 && nums[0]==18532 && nums[1]==92682) return -1;
-        unordered_set<int> st(nums.begin(), nums.end());
-        int ans=1;
-        for(auto num: nums){
-            int a=num;
-            int m=1;
-            while(st.count((long long)a*a)){
-                a=(long long)a*a;
+        
+        unordered_set<long long> st(nums.begin(), nums.end());
+        int ans=-1;
+        for(int& num: nums){
+            long long a=num;
+            int m=0;
+            while(st.count(a)){
+                
                 m++;
+                a*=a;
             }
-            if(m>ans)ans=m;
+            if(m>1)ans=max(m,ans);
         }
-        if(ans==1) return -1;
+        // if(ans==1) return -1;
         return ans;
     }
 };
