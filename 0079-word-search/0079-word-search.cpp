331b8ad5c153056f40b6e74ae38a1dab0a1ac1ff
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2024-12-19 13:39:09 +0530

    Time: 1499 ms (7.49%), Space: 11.5 MB (49.54%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool ans=false;
+    void find(int i, int j, string a, vector<vector<char>>& board,  string word,int index){
+        if(index==word.size()){
+            ans=true;
+            return ;
+        }
+        if(i<0 || j>board[0].size()-1 || j<0 || i>board.size()-1 )return;
+        if(board[i][j]=='#')return;
+        char temp=board[i][j];
+        if(word[index]!=board[i][j])return;
+        a+=word[index];
+        // cout<<word[index];
+        board[i][j]='#';
+        find(i+1,j,a,board,  word, index+1);
+        find(i-1,j,a,board,  word, index+1);
+        find(i,j+1,a,board,  word, index+1);
+        find(i,j-1,a,board,  word, index+1);
+        board[i][j]=temp;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        // vector<vector<int>> check(board.size(), vector<int>(board[0].size(),0));
+        string a="";
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(word[0]==board[i][j]){
+                    find(i,j,a,board, word,0);
+                }
+                if(ans)return true;
+            }
+        }
+        return false;;
+    }
+};
