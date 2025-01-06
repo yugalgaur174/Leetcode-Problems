65b652a82cd54a030dff98e0e2929cedc3b20b06
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-01-06 14:43:58 +0530

    Time: 3 ms (63.6%), Space: 12.2 MB (50.97%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int> ans;
-        vector<pair<int, int>>left;
-        vector<pair<int, int>>right;
-        left.push_back({0,0});
-        for(int i=0;i<boxes.size();i++){
-            auto it=left.back();
-            int a=it.first;
-            int b=it.second;
-            b+=a;
-            if(boxes[i]=='1'){
-                a++;
+        int n = boxes.size();
+        vector<int> ans(n, 0);
+        
+        int count = 0; 
+        int moves = 0; 
+        for (int i = 0; i < n; ++i) {
+            ans[i] += moves;
+            if (boxes[i] == '1') {
+                count++;
             }
-            left.push_back({a,b});
+            moves += count;
         }
-        right.push_back({0,0});
-        for(int i=boxes.size()-1;i>=0;i--){
-            auto it=right[0];
-            int a=it.first;
-            int b=it.second;
-            b+=a;
-            if(boxes[i]=='1'){
-                a++;
+
+        count = 0;
+        moves = 0;
+
+        for (int i = n - 1; i >= 0; --i) {
+            ans[i] += moves;
+            if (boxes[i] == '1') {
+                count++;
             }
-            right.insert(right.begin(), {a,b});
+            moves += count;
         }
-        for(int i=1;i<=boxes.size();i++){
-            int a=left[i].second+right[i-1].second;
-            ans.push_back(a);
-        }
+
         return ans;
     }
 };
