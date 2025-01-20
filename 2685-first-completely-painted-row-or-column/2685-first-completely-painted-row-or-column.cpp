f34c037696de41ec5c49026655dff03c2f40a43e
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-01-21 00:06:04 +0530

    Time: 321 ms (5.18%), Space: 173.7 MB (21.95%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -3,17 +3,18 @@ public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         unordered_map<int, int> col,row;
         map<int,pair<int, int>> mark;
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[0].size();j++){
+        int n=mat[0].size();
+        int m=mat.size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 mark[mat[i][j]]={i,j};
             }
         }
-        
         for(int i=0;i<arr.size();i++){
             col[mark[arr[i]].first]++;
             row[mark[arr[i]].second]++;
             // cout<<arr[i]<<" "<<mark[arr[i]].first<<" "<<mark[arr[i]].second<<endl;
-            if(col[mark[arr[i]].first]==mat[0].size() || row[mark[arr[i]].second]==mat.size()){
+            if(col[mark[arr[i]].first]==n || row[mark[arr[i]].second]==m){
                 return i;
             }
         }
