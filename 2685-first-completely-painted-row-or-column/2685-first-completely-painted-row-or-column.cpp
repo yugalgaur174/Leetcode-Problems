1d0adc09c23cd0686436f982a6920c4d5ddba304
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-01-20 23:58:10 +0530

    Time: 2329 ms (5.18%), Space: 173.5 MB (22.56%) - LeetHub

---
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        unordered_map<int, int> col,row;
+        map<int,pair<int, int>> mark;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                mark[mat[i][j]]={i,j};
+            }
+        }
+        
+        for(int i=0;i<arr.size();i++){
+            col[mark[arr[i]].first]++;
+            row[mark[arr[i]].second]++;
+            cout<<arr[i]<<" "<<mark[arr[i]].first<<" "<<mark[arr[i]].second<<endl;
+            if(col[mark[arr[i]].first]==mat[0].size() || row[mark[arr[i]].second]==mat.size()){
+                return i;
+            }
+        }
+        return 0;
+    }
+};
