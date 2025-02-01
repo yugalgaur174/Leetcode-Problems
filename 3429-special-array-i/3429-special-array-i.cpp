860dc34ca1d97c7390d832ac8d22f00502e00be2
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-02-01 14:40:18 +0530

    Time: 0 ms (100%), Space: 27.7 MB (61.71%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
         for(int i=0;i<nums.size()-1;i++){
-            cout<<nums[i]<<" "<<nums[i+1]<<endl;
+            // cout<<nums[i]<<" "<<nums[i+1]<<endl;
             if(nums[i]%2==1 && nums[i+1]%2==1){
-                cout<<nums[i]<<" "<<nums[i+1]<<endl;
+                // cout<<nums[i]<<" "<<nums[i+1]<<endl;
                 return false;
             }
             else if(nums[i]%2==0 && nums[i+1]%2==0){
-                cout<<nums[i]<<" "<<nums[i+1]<<endl;
+                // cout<<nums[i]<<" "<<nums[i+1]<<endl;
                 return false;
             }
         }
