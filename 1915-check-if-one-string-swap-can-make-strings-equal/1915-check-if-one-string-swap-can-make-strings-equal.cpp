e4d80afebdefeb4da86a99efff0b109774b0f7e8
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-02-05 18:35:59 +0530

    Time: 0 ms (100%), Space: 8.2 MB (85.97%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        if(s1==s2)return true;
-        int n1=s1.size();
-        int n2=s2.size();
-        int a=-1,b=-1;
-        for(int i=0;i<s1.size();i++){
-            if(s1[i]!=s2[i]){
-                if(a==-1){
-                    a=i;
-                }
-                else{
-                    b=i;
+        if (s1 == s2)
+            return true;
+        int n1 = s1.size();
+        int n2 = s2.size();
+        int a = -1, b = -1;
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] != s2[i]) {
+                if (a == -1) {
+                    a = i;
+                } else {
+                    b = i;
+                    char z = s1[a];
+                    s1[a] = s1[b];
+                    s1[b] = z;
+                    break;
                 }
             }
         }
-        if(a==-1 || b==-1)return false;
-        char z=s1[a];
-        s1[a]=s1[b];
-        s1[b]=z;
-        return s1==s2;
+        if (a == -1 || b == -1)
+            return false;
+
+        return s1 == s2;
     }
 };
