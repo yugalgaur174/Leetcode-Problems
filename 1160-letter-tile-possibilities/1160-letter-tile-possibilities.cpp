472ff11d3c1da3369d61181fe19f3d9f5d5efc72
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-02-17 21:47:25 +0530

    Time: 204 ms (5.23%), Space: 18.4 MB (16.89%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    set<string> st;
+     void finding(unordered_map<char, int>& mpp, string s, int n) {
+        if (!s.empty())  // Avoid inserting empty string
+            st.insert(s);
+
+        for (auto& it : mpp) { 
+            if (it.second > 0) {
+                it.second--;  
+                finding(mpp, s + it.first, n);
+                it.second++;  
+            }
+        }
+    }
+    int numTilePossibilities(string tiles) {
+        unordered_map<char, int> mpp;
+        for(int i=0;i<tiles.size();i++){
+            mpp[tiles[i]]++;
+        }    
+        finding(mpp, "",tiles.size());
+        for(auto it: st){
+            cout<<it<<" ";
+        }
+        return st.size();
+    }
+};
