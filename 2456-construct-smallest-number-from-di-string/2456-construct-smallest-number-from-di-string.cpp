4ac13f1b608b6d56e4d0c84b0753ed7624208f34
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-02-18 23:20:08 +0530

    Time: 0 ms (100%), Space: 7.9 MB (63.49%) - LeetHub

---
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string ans = "";
+
+    void findSmallest(string& s, string& pattern, bool used[10]) {
+        if (!ans.empty()) return; // Stop early if answer is found
+
+        if (s.size() == pattern.size() + 1) { // Base case: Valid number is formed
+            ans = s;
+            return;
+        }
+
+        for (int j = 1; j <= 9; j++) {
+            if (used[j]) continue;
+
+            if (!s.empty()) {
+                int prev = s.back() - '0';
+                if ((pattern[s.size() - 1] == 'I' && prev >= j) ||
+                    (pattern[s.size() - 1] == 'D' && prev <= j))
+                    continue;
+            }
+
+            used[j] = true;
+            s.push_back(j + '0');
+            findSmallest(s, pattern, used);
+            s.pop_back();
+            used[j] = false;
+        }
+    }
+
+    string smallestNumber(string pattern) {
+        bool used[10] = {false};
+        string s = "";
+        findSmallest(s, pattern, used);
+        return ans;
+    }
+};
