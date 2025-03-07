010b7644f2786903c9c9b6664d9280d483255c6a
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-03-07 12:27:09 +0530

    Time: 1878 ms (5.18%), Space: 17.6 MB (56.3%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -17,25 +17,21 @@ public:
                 arr.push_back(i);
             }
         }
-        for(int i=0;i<arr.size();i++){
-            cout<<arr[i]<<" ";
-        }
         vector<int> ans;
         if(arr.size()<2){
             ans.push_back(-1);
         ans.push_back(-1);
         return ans;
-            // return 
         }
         int m=INT_MAX;
         for(int i=arr.size()-1;i>0;i--){
             int s=arr[i]-arr[i-1];
             m=min(m,s);
         }
-        cout<<endl<<"m="<<m<<endl;
+        // cout<<endl<<"m="<<m<<endl;
         for(int i=0;i<arr.size()-1;i++){
             int s=abs(arr[i]-arr[i+1]);
-            cout<<endl<<"s="<<s;
+            // cout<<endl<<"s="<<s;
             if(s==m){
                 ans.push_back(arr[i]);
                 ans.push_back(arr[i+1]);
