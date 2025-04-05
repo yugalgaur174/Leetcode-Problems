5e52a5dcfc55c53f1bc6fadc572a7c63885a2c20
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-04-05 16:44:28 +0530

    Time: 54 ms (12.89%), Space: 31.4 MB (9.81%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int ans=0;
-    void calc(int i, vector<int> nums, int currxor){
+    // int ans=0;
+    int calc(int i, vector<int> nums, int currxor){
         if(i==nums.size()){
-            ans+=currxor;
-            return;
+            
+            return currxor;
         }
-        calc(i+1, nums, currxor^nums[i]);
-        calc(i+1, nums, currxor);
-
+        int take=calc(i+1, nums, currxor^nums[i]);
+        int nottake=calc(i+1, nums, currxor);
+        return take+nottake;
     }
     int subsetXORSum(vector<int>& nums) {
-        calc(0, nums, 0);
-        return ans;
+        return calc(0, nums, 0);
+        // return ans;
     }
 };
