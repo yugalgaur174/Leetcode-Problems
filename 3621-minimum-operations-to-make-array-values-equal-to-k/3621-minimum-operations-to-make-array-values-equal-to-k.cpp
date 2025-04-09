d2403f90117b294dd1f01f348e8a70f5d82dba2c
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-04-09 21:34:12 +0530

    Time: 12 ms (55.35%), Space: 32.5 MB (42.14%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -6,7 +6,7 @@ public:
             if(nums[i]<k){
                 return -1;
             }
-            if(nums[i]!=k){
+            else if(nums[i]>k){
                 st.insert(nums[i]);
             }
         }
