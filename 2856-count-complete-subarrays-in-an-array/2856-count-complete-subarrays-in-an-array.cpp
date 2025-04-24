8a0e8ff8e61a54537525fd3891e1e929a5af741e
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-04-24 11:56:06 +0530

    Time: 1471 ms (17.27%), Space: 483.7 MB (8.97%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -5,19 +5,15 @@ public:
         int c=0;
         for(int i=0;i<nums.size();i++){
             mpp[nums[i]]++;
-            if(mpp[nums[i]]==1){
-                c++;
-            }
         }
+        c=mpp.size();
         int ans=0;
         for(int i=0;i<nums.size();i++){
             unordered_map<int, int> mpp2;
             int m=0;
             for(int j=i;j<nums.size();j++){
                 mpp2[nums[j]]++;
-                if(mpp2[nums[j]]==1){
-                    m++;
-                }
+                m=mpp2.size();
                 if(m==c){
                     ans+=nums.size()-j;
                     break;
