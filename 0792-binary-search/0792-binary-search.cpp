a9f4b7261a63c45f6bd1dbbc61119eb253d75a29
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-05-10 15:23:36 +0530

    Time: 3 ms (2.22%), Space: 46.5 MB (9.61%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int find(int start, int end, int target, vector<int> nums){
-        if(start==end){
-            if(nums[start]==target)return start;
-            else return -1;
-        }
         int m=(start+end)/2;
-        int ans=-1;
         if(nums[m]==target)return m;
-        else if(nums[m]>target){
-             ans=find(start, m, target, nums);
+        if(start>end){
+            return -1;
+        }
+        int ans=-1;
+        if(nums[m]>target){
+             ans=find(start, m-1, target, nums);
         }
         else ans=find(m+1, end, target, nums);
         return ans;
