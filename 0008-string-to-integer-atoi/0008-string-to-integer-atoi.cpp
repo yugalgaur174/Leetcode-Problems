639eddb420e2b55c8e0ba9c5b5def53d89986513
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-05-24 14:37:07 +0530

    Time: 0 ms (100%), Space: 9.2 MB (56.84%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        long long a = 0;
-        int sign = 1;  // Default is positive
-        bool started = false, hasSign = false;
-
-        for (int i = 0; i < s.size(); i++) {
-            // Skip spaces before number starts
-            if (s[i] == ' ' && !started && !hasSign) {
-                continue;
+        long long ans=0;
+        bool nega=false;
+        bool space=false;
+        bool start=false;
+        int i=0;
+        while(s[i]==' '){
+                i++;
+        }
+        for(i;i<s.size();i++){
+            if(s[i]=='-' && !start){
+                start=true;
+                nega=true;
             }
-
-            // Handle sign if it appears and no number has started yet
-            if ((s[i] == '-' || s[i] == '+') && !started && !hasSign) {
-                sign = (s[i] == '-') ? -1 : 1;
-                hasSign = true;
+            else if(s[i]=='+' && !start){
+                start=true;
             }
-            // Handle digits
-            else if (s[i] >= '0' && s[i] <= '9') {
-                a = a * 10 + (s[i] - '0');
-                started = true;
-                
-                // Stop if the number goes out of bounds
-                if (a * sign > 2147483647) return 2147483647;
-                if (a * sign < -2147483648) return -2147483648;
-            }
-            // Stop if any non-digit character appears after the number starts
-            else if (started) {
+            else if(!isdigit(s[i])){
                 break;
             }
-            // Return 0 if an invalid character appears before any digits
-            else {
-                return 0;
+            else{
+                start=true;
+                ans=ans*10+s[i]-'0';
+                 if (!nega && ans > INT_MAX) return INT_MAX;
+                if (nega && -ans < INT_MIN) return INT_MIN;
+            
             }
         }
-        return a * sign;
+        return nega ? -ans : ans;
     }
 };
