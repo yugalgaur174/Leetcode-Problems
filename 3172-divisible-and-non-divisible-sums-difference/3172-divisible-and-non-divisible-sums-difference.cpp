6b29b815f98324e3c7f3a2fbfd2098940222bbe4
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-05-27 11:39:59 +0530

    Time: 14 ms (0.82%), Space: 8.5 MB (36.04%) - LeetHub

---
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int num1=n/m;
+        int k=num1;
+        num1=0;
+        for(int i=0;i<k;i++){
+            num1+=(i+1)*m;
+            cout<<num1<<endl;
+        }
+        int num2=(n*(n+1))/2;
+        num2-=num1;
+        cout<<num2<<" "<<num1;
+        return num2-num1; 
+
+    }
+};
