2b34a9e85eecd822f95df59d169897241f3f8d4f
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-05-31 23:06:35 +0530

    Time: 0 ms (100%), Space: 18.3 MB (35.47%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n=image.size();
-        int m=image[0].size();
-        if(image[sr][sc]==color) return image;
-        queue<pair<int,int>> q;
-        
-        q.push({sr,sc});
-       int mark=image[sr][sc];
-        image[sr][sc]=color;
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
+    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
+        queue<pair<int, int>> q;
+        int original = grid[sr][sc];
+        if (original == color) return grid;
+        q.push(make_pair(sr,sc));
+        vector<int> row = {0, -1, 0, 1 };
+        vector<int> col = { 1, 0, -1, 0 };
         while(!q.empty()){
-            int r=q.front().first;
-            int c=q.front().second;
-            image[r][c]=color;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && ncol>=0 && nrow<n&& ncol<m&& image[nrow][ncol]==mark){
-                    q.push({nrow,ncol});
-                    // vis[nrow][ncol]=color;
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                auto it=q.front();
+                q.pop();
+                int a=it.first;
+                int b=it.second;
+                for(int j=0;j<4;j++){
+                    int r=a+row[j];
+                    int c=b+col[j];
+                    if(r<0 || c<0 || r>=grid.size() || c>=grid[0].size()){
+                        continue;
+                    }
+                    else{
+                        if(grid[r][c]==original){
+                            grid[r][c]=color;
+                            q.push(make_pair(r,c));
+                        }
+                    }
                 }
             }
         }
-        return image;
+        grid[sr][sc]=color;
+        return grid;
     }
 };
