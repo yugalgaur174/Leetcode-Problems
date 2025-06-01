d23ad7b464822d8d9be6db5bf256581127d61ba6
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-06-01 14:39:15 +0530

    Time: 24 ms (55.88%), Space: 8.9 MB (84.71%) - LeetHub

---
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    #define ll long long
+    long long distributeCandies(int n, int limit) {
+        ll ans=0;
+        for(int i=0;i<=min(n,limit);i++){
+            ll rem=n-i;
+            ll low=max(0ll,rem-limit);
+            ll high=min(rem,(ll)limit);
+            if(low<=high){
+                ans+=(high-low+1);
+            }
+        }
+        return ans;
+    }
+};
