046d27417650c87cd695805add065503740c0098
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-06-11 01:02:50 +0530

    Time: 21 ms (7.89%), Space: 10.4 MB (45.16%) - LeetHub

---
+++ 2173-number-of-valid-words-in-a-sentence/2173-number-of-valid-words-in-a-sentence.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int countValidWords(string sentence) {
+        stringstream ss(sentence);
+        string word;
+        int ans = 0;
+        while (ss >> word) {
+            bool valid = true;
+            int cnt = 0;
+            for (int i = 0; i < word.size(); i++) {
+                if (isdigit(word[i])) {
+                    valid = false;
+                    break;
+                } else if (word[i] == '-' && valid) {
+                    cnt++;
+                    if (i - 1 >= 0 && i + 1 < word.size() && cnt <= 1) {
+                        if (word[i - 1] >= 'a' && word[i - 1] <= 'z' &&
+                            word[i + 1] >= 'a' && word[i + 1] <= 'z') {
+                            continue;
+                        } else {
+                            valid = false;
+                    break;
+
+                        }
+                    } else {
+                        valid = false;
+                    break;
+
+                    }
+                } else if (word[i] == '!' || word[i] == ',' || word[i] == '.') {
+                    if (i == word.size() - 1) {
+                        valid = true;
+                    } else {
+                        valid = false;
+                    break;
+
+                    }
+                }
+            }
+            if (valid) {
+                cout << word << endl;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
