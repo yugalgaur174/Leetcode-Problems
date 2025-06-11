b7212c1b15259057c4b3238e682a83e96c2a7afb
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-06-11 15:48:10 +0530

    Time: 16 ms (47.44%), Space: 16.6 MB (84.49%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        int g[n][n];
-        int dist[n];
-        bool vis[n];
-        memset(g, 0x3f, sizeof(g));
-        for (auto& e : edges) {
-            int f = e[0], t = e[1], w = e[2];
-            g[f][t] = g[t][f] = w;
+        vector<vector<int>> dist(n, vector<int> (n,INT_MAX));
+        for(int i=0;i<edges.size();i++){
+            int a=edges[i][0];
+            int b=edges[i][1];
+            int c=edges[i][2];
+            dist[a][b]=c;
+            dist[b][a]=c;
         }
-        auto dijkstra = [&](int u) {
-            memset(dist, 0x3f, sizeof(dist));
-            memset(vis, 0, sizeof(vis));
-            dist[u] = 0;
-            for (int i = 0; i < n; ++i) {
-                int k = -1;
-                for (int j = 0; j < n; ++j) {
-                    if (!vis[j] && (k == -1 || dist[j] < dist[k])) {
-                        k = j;
+        for(int i = 0; i < n; i++) {
+            dist[i][i] = 0;
+        }
+        for(int via=0;via<n;via++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][via]!=INT_MAX && dist[via][j]!=INT_MAX && dist[i][j]>dist[i][via]+dist[via][j]){
+                        dist[i][j]=dist[i][via]+dist[via][j];
+                        dist[j][i]=dist[i][via]+dist[via][j];
+
                     }
                 }
-                vis[k] = true;
-                for (int j = 0; j < n; ++j) {
-                    dist[j] = min(dist[j], dist[k] + g[k][j]);
-                }
-            }
-            return count_if(dist, dist + n, [&](int d) { return d <= distanceThreshold; });
-        };
-        int ans = n, cnt = n + 1;
-        for (int i = n - 1; ~i; --i) {
-            int t = dijkstra(i);
-            if (t < cnt) {
-                cnt = t;
-                ans = i;
             }
         }
-        return ans;
+        vector<int> arr(n,0);
+        int m=INT_MAX;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(dist[i][j]<=distanceThreshold){
+                    arr[i]++;
+                }
+            }
+                    if(m>arr[i]){
+                        m=arr[i];
+                    }
+        }
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]==m){
+                return i;
+            }
+        }
+        return 0;
     }
 };
