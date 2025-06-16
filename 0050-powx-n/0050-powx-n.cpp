773eeff0c49c9cd7119ae511fc2d42fc44161440
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-06-16 17:38:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (42.88%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
+    double power(double x, long long n) {
+        if(n == 0) {
+            return 1;
+        }
+        double half=power(x, n / 2);
+        if(n%2==0){
+            return half*half;
+        }
+        else{
+            return x*half*half;
+        }
+      
+        
+        
+    }
+
     double myPow(double x, int n) {
-        if(n==0){
-            return 1.0;
+        if(n == 1) return x;
+        if(n == -1) return 1.0 / x;
+
+        long long exp = n; 
+        if(exp < 0) {
+            return 1.0 / power(x, -exp);
         }
-        long long N=n;
-        if(n<0){
-            x=1/x;
-            N=-N;
-        }
-        double half = myPow(x, N / 2);
-        if(N%2==0) return half*half;
-        // if(half*half*x=='inf'){
-        //     return 0.00;
-        // }
-        return half*half*x;
+        return power(x, exp);
     }
 };
