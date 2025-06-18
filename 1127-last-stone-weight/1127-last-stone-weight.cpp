5924a3b58a8562856eeb99734a24b522d6f799aa
YUGAL GAUR <156823053+yugalgaur174@users.noreply.github.com>
2025-06-18 23:47:37 +0530

    Time: 0 ms (100%), Space: 9.9 MB (77.09%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(auto &it: stones){
+            pq.push(it);
+        }
+        while(pq.size()>1){
+            int a=pq.top();
+            pq.pop();
+            int b=pq.top();
+            pq.pop();
+            if(a>b){
+                pq.push(a-b);
+            }
+        }
+        if(pq.empty()){
+            return 0;
+        }
+        return pq.top();
+    }
+};
